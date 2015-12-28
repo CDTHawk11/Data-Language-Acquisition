0cd8077541936693e9c150fc71a03cd94d2c250c
CDTHawk11 <jlancecunningham@gmail.com>
2015-12-28 17:18:38 -0600

    Added for loop.
    Added the passage of the list from the myscript.js to the background scripts

--- public_html/background.js
+++ public_html/background.js
@@ -5,16 +5,18 @@ chrome.runtime.onMessage.addListener(
             var my_Key = "AIzaSyDGcpNr1_IzF5aEeS5TIF8Sf7NFpBBtjf8"
             var orignal_text = request.to_Translate;
             
-            $.ajax({
-                type: "GET",
-                url: "https://www.googleapis.com/language/translate/v2?",
-                data: {key: my_Key, source: "en", target: "fr", q: orignal_text},
-                dataType: 'json',
-                success: function (result, status, xhr) {
-                    alert(result.data.translations[0].translatedText);
-                },
-                error: function (xhr, status, errorMsg) {
-                    alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
-                }
-            });
+            for (word in original_text) {
+            
+                $.ajax({
+                    type: "GET",
+                    url: "https://www.googleapis.com/language/translate/v2?",
+                    data: {key: my_Key, source: "en", target: "es", q: word},
+                    dataType: 'json',
+                    success: function (result, status, xhr) {
+                        alert(result.data.translations[0].translatedText);
+                    },
+                    error: function (xhr, status, errorMsg) {
+                        alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+                    }
+                });
         });
