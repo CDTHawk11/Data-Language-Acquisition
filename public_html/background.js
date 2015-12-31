668c36d1d94305537f2d3b3a722b7cee05f7a19e
smdev15 <sumanm.82@gmail.com>
2015-12-30 23:34:54 -0600

    Updated background script to send the translation back to content script

--- public_html/background.js
+++ public_html/background.js
@@ -1,29 +1,42 @@
+
 chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
 
-            var my_Key = "AIzaSyDGcpNr1_IzF5aEeS5TIF8Sf7NFpBBtjf8";
-            var original_text = request.to_Translate;
-            var translated = [];
-            
-            for (word in original_text) {
-                $.ajax({
-                    type: "GET",
-                    url: "https://www.googleapis.com/language/translate/v2?",
-                    data: {key: my_Key, source: "en", target: "es", q: original_text[word]},
-                    dataType: 'json',
-                    success: function (result, status, xhr) {
-                        translated.push(result.data.translations[0].translatedText);
-                    },
-                    error: function (xhr, status, errorMsg) {
-                        alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
-                    }
-                }).done(function() {
-                    if(translated.length === original_text.length){
-                        alert(translated);
-                        sendResponse({translated_words: JSON.stringify(translated)});
-                    } 
-                });
-            }
+            var deferred = $.Deferred();
+
+            translate(request.to_Translate, deferred);
+
+            deferred.done(function (translation) {
+                sendResponse({translated_words: translation});
+            });
+
+            return true;
         });
-       
-        
+
+function translate(original_text, dfrd) {
+
+    var my_Key = "AIzaSyDGcpNr1_IzF5aEeS5TIF8Sf7NFpBBtjf8";
+    var translated = [];
+
+    for (word in original_text) {
+
+        $.ajax({
+            type: "GET",
+            url: "https://www.googleapis.com/language/translate/v2?",
+            data: {key: my_Key, source: "en", target: "es", q: original_text[word]},
+            dataType: 'json',
+            success: function (result, status, xhr) {
+                translated.push(result.data.translations[0].translatedText);
+
+                if (translated.length === original_text.length) {
+                    dfrd.resolve(JSON.stringify(translated));
+                }
+            },
+            error: function (xhr, status, errorMsg) {
+                alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+            }
+
+        });
+    }
+}
+
