af686c691aebb8406480250f5f9f5e6767ec887a
smdev15 <sumanm.82@gmail.com>
2016-01-04 00:34:35 -0600

    Updated script to change the slider value into a user property that can be persisted through browser sessions and also used across other files

--- public_html/script.js
+++ public_html/script.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
-    $('#application-progress').slider({
-        range: "min",
-        value: 10,
-        min: 0,
-        max: 100,
-        animate: true,
-        slide: function (event, ui) {
-            $("#progress").val(ui.value + "%");
-            //$("#progress").html(ui.value + "%");
-        }
-    });
-    $("#progress").val($("#application-progress").slider("value") + "%");
-    //$("#progress").html($(".application-progress").slider("value") + "%");
-    /*
-    $("input, select").change(function () {
-        var percentage = 0;
-        $('input, select').each(function () {
-            if ($.trim(this.value) != "")
-                percentage += 10;
-        });
-        $(".application-progress").slider("value", percentage);
 
-        $("#progress").html(percentage + "%");
+    var tranLimit = "10%";
+
+    chrome.storage.sync.get("TRAN_LIMIT", function (limit) {
+        
+        if (!(limit["TRAN_LIMIT"])) {
+            chrome.storage.sync.set({'TRAN_LIMIT': tranLimit}, function () {
+            });
+        } else {
+            tranLimit = limit["TRAN_LIMIT"];
+        }
+        
+        $('#application-progress').slider({
+            range: "min",
+            min: 0,
+            value: parseInt(tranLimit),
+            max: 100,
+            animate: true,
+            slide: function (event, ui) {
+                $("#progress").val(ui.value + "%");
+            },
+            change: function (event, ui) {
+                var translationLimit = $("#progress").val();
+                chrome.storage.sync.set({'TRAN_LIMIT': translationLimit}, function () {
+                });
+            }
+        });
+
+        $("#progress").val($("#application-progress").slider("value") + "%");
+        
     });
-    */
+    
 });
 
