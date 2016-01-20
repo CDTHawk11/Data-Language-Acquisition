633543bf1b9506b7b05b56950eb28442675d6403
Charles <Charles@DESKTOP-RH3UFO7.att.net>
2016-01-19 20:22:41 -0600

    Added some basic commenting

--- public_html/myscript.js
+++ public_html/myscript.js
@@ -1,43 +1,51 @@
-function getText(){
+// This function gets all the text in browser
+function getText() {
     return document.body.innerText;
 }
 
-var str=getText();
-var words=str.split(" ");
+var str = getText(); //stores into browser text into variable
+var words = str.split(" "); //make a list of the words
 //console.log(words);
 
 var counts = [];
 
-for(var i = 0; i< words.length; i++) {
+//counts number of words
+for (var i = 0; i < words.length; i++) {
     var num = words[i];
-    counts[num] = counts[num] ? counts[num]+1 : 1;
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
 }
 
 console.log(counts);
 
-var unique_numbers=Object.keys(counts).length;
+var unique_numbers = Object.keys(counts).length; //number unique words that appear on page
 
-console.log(unique_numbers);
+console.log(unique_numbers); 
 
-var counts_new=[];
+//orders words from most occuring to least
+var counts_new = [];
 for (var key in counts)
-	counts_new.push([key,counts[key]]);
-counts_new.sort(function(a, b) {return b[1] - a[1]});
+    counts_new.push([key, counts[key]]);
+counts_new.sort(function (a, b) {
+    return b[1] - a[1]
+});
 
 console.log(counts_new);
-var arrays_to=counts_new.splice(0,4);
 
+var arrays_to = counts_new.splice(0, 4); //four most occuring words (temporary)
 
-words_to=[];
+//Extracts most words from the dictionary and places them in a list
+words_to = [];
 for (var key in arrays_to)
-	words_to.push([arrays_to[key][0]]);
+    words_to.push([arrays_to[key][0]]);
 var to_Translate = [].concat.apply([], words_to);
+
 console.log(to_Translate);
 
-var json_to_Translate=JSON.stringify(to_Translate),
-    json_parse=JSON.parse(json_to_Translate);
+//packaging list of words to be translated in JSON for transfer to background scripts
+var json_to_Translate = JSON.stringify(to_Translate),
+        json_parse = JSON.parse(json_to_Translate);
 
-alert(json_parse);
+console.log(json_parse);
 
 //$.ajax({
 //    type: "POST",
@@ -57,20 +65,23 @@ alert(json_parse);
 //    }
 //}
 
-wordDict={"the":"piano","and":"Hello", "a":"huh?"};
+wordDict = {"the": "piano", "and": "Hello", "a": "huh?"};
 
 //$('body *').each(function (){
 //    $(this).text();
 //});
 
 var items = document.getElementsByTagName("*");
-    for (var i=0; i < items.length; i++) {
-        if (items[i].innerText && items[i].innerText != "") {
-            for (word in wordDict) {
-                items[i].innerText = items[i].innerText.replace(new RegExp('\\b' + word + '\\b',"gi"), wordDict[word]);
-            }
+console.log(items);
+for (var i = 0; i < items.length; i++) {
+    if (items[i].innerText && items[i].innerText != "") {
+        for (word in wordDict) {
+            items[i].innerText = items[i].innerText.replace(new RegExp('\\b' + word + '\\b', "gi"), wordDict[word]);
         }
+    }else{
+        console.log("complete");
     }
+}
 
 //for (word in wordDict) {
 //    document.body.innerText = document.body.innerText.replace(new RegExp('\\b' + word + '\\b',"gi"), wordDict[word]);
