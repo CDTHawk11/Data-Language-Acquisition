f5aa69e6505960d0cbf65252023d7aa01f892995
CDTHawk11 <jlancecunningham@gmail.com>
2016-02-02 12:26:37 -0600

    Updated popup with questionnaire.  Popup submits to server.  Popup is still failing to disappear on click of submit.

--- public_html/background.js
+++ public_html/background.js
@@ -14,7 +14,7 @@ function translate(original_text, dfrd) {
 	var jsonParameter = {"q":original_text};
     $.ajax({
         type: "POST",
-        url: "http://localhost:8080/translator/rest/trans/es",
+        url: "http://ec2-52-11-228-170.us-west-2.compute.amazonaws.com:8080/translator/rest/trans/es",
         data: JSON.stringify(jsonParameter),
         contentType: "application/json",
         headers: {"Accept": "application/json"},
@@ -28,3 +28,36 @@ function translate(original_text, dfrd) {
 
     });
 }
+/*
+function translate(original_text, dfrd) {
+    
+    var my_Key = "AIzaSyDGcpNr1_IzF5aEeS5TIF8Sf7NFpBBtjf8";
+    var translated = [];
+
+    for (word in original_text) {
+        
+        $.ajax({
+            type: "GET",
+            url: "https://www.googleapis.com/language/translate/v2?",
+            data: {key: my_Key, source: "en", target: "es", q: original_text[word]},
+            dataType: 'json',
+            success: function (result, status, xhr) {
+                translated.push(result.data.translations[0].translatedText);
+
+                if (translated.length === original_text.length) {
+                    var merged = {};
+                    for (word in original_text) {
+                        merged[original_text[word]] = translated[word];
+                    }
+                    
+                    dfrd.resolve(JSON.stringify(merged));
+                }
+            },
+            error: function (xhr, status, errorMsg) {
+                alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+            }
+
+        });
+    }
+}
+*/
--- public_html/myscript.js
+++ public_html/myscript.js
@@ -3,95 +3,99 @@ function getText() {
     return document.body.innerText;
 }
 
-var str = getText(); //stores into browser text into variable
-var words = str.split(" "); //make a list of the words
-//console.log(words);
+// Get the saved limit of translation that has been configured through slider
+// and saved within user's computer.
+var tranLimit = 10;
+chrome.storage.sync.get("TRAN_LIMIT", function (limit) {
+	if(limit["TRAN_LIMIT"]) {
+		tranLimit = parseInt(limit["TRAN_LIMIT"]);
+	}
+	
+	var allText = getText(); // stores into browser text into variable
+	var words = allText.match((/\b[a-zA-Z]+\b/g)); // filter all text to only words
+	
+	var counts = [];
 
-var counts = [];
+	// counts number of words
+	for (var i = 0; i < words.length; i++) {
+	    var num = words[i];
+	    counts[num] = counts[num] ? counts[num] + 1 : 1;
+	}
 
-//counts number of words
-for (var i = 0; i < words.length; i++) {
-    var num = words[i];
-    counts[num] = counts[num] ? counts[num] + 1 : 1;
+	console.log(counts);
+
+	var unique_numbers = Object.keys(counts).length; // number unique words that
+														// appear on page
+
+	console.log(unique_numbers); 
+
+	// orders words from most occurring to least
+	var counts_new = [];
+	for (var key in counts)
+	    counts_new.push([key, counts[key]]);
+	counts_new.sort(function (a, b) {
+	    return b[1] - a[1]
+	});
+
+	console.log(counts_new);
+
+	var arrays_to = counts_new.splice(0, 4); // four most occurring words
+												// (temporary)
+
+	// Extracts the most frequently occurring words from the arrays_to dictionary and places them in a list
+	words_to = [];
+	for (var key in arrays_to)
+	    words_to.push([arrays_to[key][0]]);
+	var to_Translate = [].concat.apply([], words_to);
+
+	conjugations=[];
+	
+//	for (t in to_Translate)
+//		for (i in words)
+//			if (to_Translate[t]==words[i] && words[i+1]=='undefined') {
+//				conjugations.push(to_Translate[t].concat(" "+words[i]));
+//			}	else if (to_Translate[t]==words[i] && words[i+1] != 'undefined') {
+//					conjugations.push(to_Translate[t].concat(" "+words[i+1]));
+//			}
+	
+	
+//	console.log(conjugations);
+
+	// packaging list of words to be translated in JSON for transfer to background
+	// scripts
+	var json_to_Translate = JSON.stringify(to_Translate),
+	        json_parse = JSON.parse(json_to_Translate);
+
+	console.log(json_parse);
+
+	chrome.runtime.sendMessage({json_parse}, function(response) {  
+	    replaceText(response.merged_words);
+	});
+});
+
+function replaceText(jsonArr) {
+	$("body *").textFinder(function() {
+		for (var key in jsonArr) {
+			
+			var matcher = new RegExp('\\b' + key + '\\b', "gi");
+			//var replacer = jsonArr[key].split(" ");
+			this.data = this.data.replace(matcher, jsonArr[key]);
+			
+		}
+	});
 }
 
-console.log(counts);
-
-var unique_numbers = Object.keys(counts).length; //number unique words that appear on page
-
-console.log(unique_numbers); 
-
-//orders words from most occuring to least
-var counts_new = [];
-for (var key in counts)
-    counts_new.push([key, counts[key]]);
-counts_new.sort(function (a, b) {
-    return b[1] - a[1]
-});
-
-console.log(counts_new);
-
-var arrays_to = counts_new.splice(0, 4); //four most occuring words (temporary)
-
-//Extracts most words from the dictionary and places them in a list
-words_to = [];
-for (var key in arrays_to)
-    words_to.push([arrays_to[key][0]]);
-var to_Translate = [].concat.apply([], words_to);
-
-console.log(to_Translate);
-
-//packaging list of words to be translated in JSON for transfer to background scripts
-var json_to_Translate = JSON.stringify(to_Translate),
-        json_parse = JSON.parse(json_to_Translate);
-
-console.log(json_parse);
-
-//$.ajax({
-//    type: "POST",
-//    url: "ajax/count.php",
-//    data: { words_for_translation: json_parse },
-//    dataType: 'json',
-//    success: function(data){
-//    
-//    }
-//});
-
-//$.getJSON( "/url", {params }, function( data, status, xhr ) {
-//    $.each(data.response, function(resKey, resValue){
-//        if(resKey == "success"){
-//             var _result = JSON.parse(resValue);
-//        }
-//    }
-//}
-
-chrome.runtime.sendMessage({json_parse}, function(response) {  
-    alert(JSON.stringify(response.merged_words));
-});
-
-/*
-
-wordDict = {"the": "piano", "and": "Hello", "a": "huh?"};
-
-//$('body *').each(function (){
-//    $(this).text();
-//});
-
-var items = document.getElementsByTagName("*");
-console.log(items);
-for (var i = 0; i < items.length; i++) {
-    if (items[i].innerText && items[i].innerText != "") {
-        for (word in wordDict) {
-            items[i].innerText = items[i].innerText.replace(new RegExp('\\b' + word + '\\b', "gi"), wordDict[word]);
+// jQuery plugin to find and replace text
+jQuery.fn.textFinder = function( fn ) {
+    this.contents().each( scan );
+    // callback function to scan through the child nodes recursively
+    function scan() {
+        var node = this.nodeName.toLowerCase();
+        if( node === '#text' ) {
+            fn.call( this );
+        } else if( this.nodeType === 1 && this.childNodes && this.childNodes[0] && node !== 'script' && node !== 'textarea' ) {
+            $(this).contents().each( scan );
         }
-    }else{
-        console.log("complete");
     }
-}
-
-//for (word in wordDict) {
-//    document.body.innerText = document.body.innerText.replace(new RegExp('\\b' + word + '\\b',"gi"), wordDict[word]);
-//    document.body.innerText = document.body.innerText.replace(word,"gi"), wordDict[word])
-//};
-
-*/
+    return this;
+};
