6752ff6ab044c93e5fd5562cb5db1e8791d08ba4
smdev15 <sumanm.82@gmail.com>
2016-02-05 02:42:37 -0600

    Completed Java, HTML & JS changes to save feedback and integrate with DynamoDB

--- public_html/script.js
+++ public_html/script.js
@@ -35,26 +35,30 @@ $(document).ready(function() {
 
 	});
 
+	// Attach a submit handler to feedback form
 	$('#feedbackForm').submit(function(event) {
+		
 		event.preventDefault();
+		
+		var array = $(this).serializeArray();
+		var jsonParameter = {};
+		
+		jQuery.each(array, function() {
+			jsonParameter[this.name] = this.value || '';
+	    });
+		
 		$.ajax({
 			url : "http://ec2-52-35-34-105.us-west-2.compute.amazonaws.com:8080/translator/rest/submit/feedback",
 			type : "POST",
-			contentType: "application/x-www-form-urlencoded",
-			data : $('#feedbackForm').serialize(),
-			success : function(result, status, xhr) {
-				alert(result);
-				$('#feedbackDiv').html(result);
-			}
+	        data: JSON.stringify(jsonParameter),
+	        contentType: "application/json",
+	        headers: {"Accept": "application/json"},
+			success: function(result, status, xhr) {
+				$("#feedbackDiv").html(result['message']);
+			},
+			error: function (xhr, status, errorMsg) {
+	            alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+	        }
 		});
 	});
 });
-
-function disappear(event){
-	  
-	event.preventDefault();
-	  
-	document.GetElementsByClassName("question").innerHTML="<p>Thanks you!</p>";
-		  
-	return false;	  
-	 };
