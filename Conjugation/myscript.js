85dbb6163071f1bc1cd765acf35cb1fc865d4289
CDTHawk11 <jlancecunningham@gmail.com>
2016-02-27 19:48:33 -0600

    Suman, this is the code with my logs to figure out what is going on!

--- Conjugation/myscript.js
+++ Conjugation/myscript.js
@@ -186,31 +186,78 @@ function replaceText(jsonArr) {
 		}
 	});
 	
-	function findText(element, pattern, callback) {
-	    for (var childi= element.childNodes.length; childi-->0;) {
-	        var child= element.childNodes[childi];
-	        if (child.nodeType==1) {
-	            findText(child, pattern, callback);
-	        } else if (child.nodeType==3) {
-	            var matches= [];
-	            var match;
-	            while (match= pattern.exec(child.data))
-	                matches.push(match);
-	            for (var i= matches.length; i-->0;)
-	                callback.call(window, child, matches[i]);
-	        }
-	    }
-	}
-
 	for (d in makeArray){
-		var highlighter = new RegExp('\\b' + makeArray[d][1] + '\\b', "g");
-		findText(document.body, highlighter, function(node, match) {
-			var span= document.createElement('span');
-			span.style.backgroundColor = "yellow";
-			node.splitText(match.index+makeArray[d][1].length);
-			span.appendChild(node.splitText(match.index));
-			node.parentNode.insertBefore(span, node.nextSibling);
-		});
+	
+	wrapText(document.body, makeArray[d][1]);
+
+	function wrapText(container, text) {
+		  // Construct a regular expression that matches text at the start or end of a string or surrounded by non-word characters.
+		  // Escape any special regex characters in text.
+		  var textRE = new RegExp('(^|\\W)' + text.replace(/[\\^$*+.?[\]{}()|]/, '\\$&') + '($|\\W)', 'm');
+		  var nodeText;
+		  var nodeStack = [];
+
+		  // Remove empty text nodes and combine adjacent text nodes.
+		  container.normalize();
+
+		  // Iterate through the container's child elements, looking for text nodes.
+		  var curNode = container.firstChild;
+
+		  while (curNode != null) {
+		    if (curNode.nodeType == Node.TEXT_NODE) {
+		      // Get node text in a cross-browser compatible fashion.
+		      if (typeof curNode.textContent == 'string')
+		        nodeText = curNode.textContent;
+		      else
+		        nodeText = curNode.innerText;
+
+		      // Use a regular expression to check if this text node contains the target text.
+		      var match = textRE.exec(nodeText);
+		      if (match != null) {
+		        // Create a document fragment to hold the new nodes.
+		        var fragment = document.createDocumentFragment();
+
+		        // Create a new text node for any preceding text.
+		        if (match.index > 0)
+		        	fragment.appendChild(document.createTextNode(match.input.substr(0, match.index)));
+		        	console.log(match.index);
+		        	console.log(match);
+		        	console.log(match.input.substr(0));
+		        	console.log(match.input.substr(0, match.index));
+		        	console.log(match[0]);
+		        // Create the wrapper span and add the matched text to it.
+		        var spanNode = document.createElement('span');
+		        spanNode.style.backgroundColor = "blue";
+		        spanNode.appendChild(document.createTextNode(match[0]));
+		        //console.log(spanNode.appendChild(document.createTextNode(match[0])));
+		        fragment.appendChild(spanNode);
+		        //console.log(fragment.appendChild(spanNode));
+		        // Create a new text node for any following text.
+		        if (match.index + match[0].length < match.input.length)
+		          fragment.appendChild(document.createTextNode(match.input.substr(match.index + match[0].length)));
+
+		        // Replace the existing text node with the fragment.
+		        curNode.parentNode.replaceChild(fragment, curNode);
+
+		        curNode = spanNode;
+		      }
+		    } else if (curNode.nodeType == Node.ELEMENT_NODE && curNode.firstChild != null) {
+		      nodeStack.push(curNode);
+		      curNode = curNode.firstChild;
+		      // Skip the normal node advancement code.
+		      continue;
+		    }
+
+		    // If there's no more siblings at this level, pop back up the stack until we find one.
+		    while (curNode != null && curNode.nextSibling == null)
+		      curNode = nodeStack.pop();
+
+		    // If curNode is null, that means we've completed our scan of the DOM tree.
+		    // If not, we need to advance to the next sibling.
+		    if (curNode != null)
+		      curNode = curNode.nextSibling;
+		  }
+		}
 	}
 }
 
@@ -231,69 +278,5 @@ jQuery.fn.textFinder = function( fn ) {
     return this;
 };
 
-/*
-wrapText(document.body, '');
 
-function wrapText(container, text) {
-	  // Construct a regular expression that matches text at the start or end of a string or surrounded by non-word characters.
-	  // Escape any special regex characters in text.
-	  var textRE = new RegExp('(^|\\W)' + text.replace(/[\\^$*+.?[\]{}()|]/, '\\$&') + '($|\\W)', 'm');
-	  var nodeText;
-	  var nodeStack = [];
 
-	  // Remove empty text nodes and combine adjacent text nodes.
-	  container.normalize();
-
-	  // Iterate through the container's child elements, looking for text nodes.
-	  var curNode = container.firstChild;
-
-	  while (curNode != null) {
-	    if (curNode.nodeType == Node.TEXT_NODE) {
-	      // Get node text in a cross-browser compatible fashion.
-	      if (typeof curNode.textContent == 'string')
-	        nodeText = curNode.textContent;
-	      else
-	        nodeText = curNode.innerText;
-
-	      // Use a regular expression to check if this text node contains the target text.
-	      var match = textRE.exec(nodeText);
-	      if (match != null) {
-	        // Create a document fragment to hold the new nodes.
-	        var fragment = document.createDocumentFragment();
-
-	        // Create a new text node for any preceding text.
-	        if (match.index > 0)
-	          fragment.appendChild(document.createTextNode(match.input.substr(0, match.index)));
-
-	        // Create the wrapper span and add the matched text to it.
-	        var spanNode = document.createElement('span');
-	        spanNode.appendChild(document.createTextNode(match[0]));
-	        fragment.appendChild(spanNode);
-
-	        // Create a new text node for any following text.
-	        if (match.index + match[0].length < match.input.length)
-	          fragment.appendChild(document.createTextNode(match.input.substr(match.index + match[0].length)));
-
-	        // Replace the existing text node with the fragment.
-	        curNode.parentNode.replaceChild(fragment, curNode);
-
-	        curNode = spanNode;
-	      }
-	    } else if (curNode.nodeType == Node.ELEMENT_NODE && curNode.firstChild != null) {
-	      nodeStack.push(curNode);
-	      curNode = curNode.firstChild;
-	      // Skip the normal node advancement code.
-	      continue;
-	    }
-
-	    // If there's no more siblings at this level, pop back up the stack until we find one.
-	    while (curNode != null && curNode.nextSibling == null)
-	      curNode = nodeStack.pop();
-
-	    // If curNode is null, that means we've completed our scan of the DOM tree.
-	    // If not, we need to advance to the next sibling.
-	    if (curNode != null)
-	      curNode = curNode.nextSibling;
-	  }
-	}
-*/
