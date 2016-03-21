268c03d3f8942b40af6fe92102b99cd232e0d768
allosaurus <allisonhwang@uchicago.edu>
2016-03-20 23:38:33 -0700

    Update files in chrome-extension folder; fixed multiple spans bug

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,10 +1,49 @@
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details) {
+	chrome.windows.getCurrent(function(win) {
+		if (details.reason == "install") {
+			var tran_data = ["TRAN_TARGET", "TRAN_LEVEL"];
+			chrome.storage.sync.remove(tran_data, function() {
+				var leftPos = win.left + win.width - 635;
+				var topPos = win.top + 70;
+		        chrome.windows.create({
+		            type: "popup",
+		            width: 610,
+		            height: 440,
+		            top: topPos,
+		            left: leftPos,
+		            focused: true
+		        }, function(window) {
+		        	chrome.tabs.create({
+		                url: "http://ec2-52-35-34-105.us-west-2.compute.amazonaws.com:8080/translator/rest/user/profile",
+		                windowId: window.id
+		            });
+		        });
+			});
+	    } else if(details.reason == "update") {
+	        var thisVersion = chrome.runtime.getManifest().version;
+	        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+	    }
+	});    		
+});
+
 chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
 
             var deferred = $.Deferred();
             translate(request.json_parse, deferred);
             deferred.done(function (merged) {
-                sendResponse({merged_words: merged});
+                chrome.storage.sync.get("SPKESY_TRAN", function (obj) {
+                	if (!(obj["SPKESY_TRAN"]) || obj["SPKESY_TRAN"] == "") {
+                		chrome.storage.sync.set({"SPKESY_TRAN":"ON"}, function() {
+                		});
+                        sendResponse({merged_words: merged});
+                	} else if (obj["SPKESY_TRAN"] === "ON") {
+                		sendResponse({merged_words: merged});
+                	} else if (obj["SPKESY_TRAN"] === "OFF") {
+                		return false;
+                	}    
+                });
             });
 
             return true;
@@ -12,19 +51,29 @@ chrome.runtime.onMessage.addListener(
 
 function translate(original_text, dfrd) {
 	var jsonParameter = {"q":original_text};
-    $.ajax({
-        type: "POST",
-        url: "http://ec2-52-35-34-105.us-west-2.compute.amazonaws.com:8080/translator/rest/trans/es",
-        data: JSON.stringify(jsonParameter),
-        contentType: "application/json",
-        headers: {"Accept": "application/json"},
-        dataType: "json",
-        success: function (result, status, xhr) {
-               dfrd.resolve(result);
-        },
-        error: function (xhr, status, errorMsg) {
-            alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
-        }
+	var targetURL = "http://ec2-52-35-34-105.us-west-2.compute.amazonaws.com:8080/translator/rest/trans/";
+	
+    chrome.storage.sync.get("TRAN_TARGET", function (obj) {
+    	if (!(obj["TRAN_TARGET"]) || obj["TRAN_TARGET"] == "") {
+    		return false;
+    	} else {
+    		targetURL = targetURL + obj["TRAN_TARGET"];
 
+    	    $.ajax({
+    	        type: "POST",
+    	        url: targetURL,
+    	        data: JSON.stringify(jsonParameter),
+    	        contentType: "application/json",
+    	        headers: {"Accept": "application/json"},
+    	        dataType: "json",
+    	        success: function (result, status, xhr) {
+    	               dfrd.resolve(result);
+    	        },
+    	        error: function (xhr, status, errorMsg) {
+    	            alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+    	        }
+    	    });
+    	}
     });
+	
 }
--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -1,43 +1,115 @@
-$(document).ready(function () {
-
-    var tranLimit = "10%";
-
-    chrome.storage.sync.get("TRAN_LIMIT", function (limit) {
-        
-        if (!(limit["TRAN_LIMIT"])) {
-            chrome.storage.sync.set({'TRAN_LIMIT': tranLimit}, function () {
-            });
-        } else {
-            tranLimit = limit["TRAN_LIMIT"];
-        }
-        
-        $('#application-progress').slider({
-            range: "min",
-            min: 0,
-            value: parseInt(tranLimit),
-            max: 100,
-            animate: true,
-            slide: function (event, ui) {
-                $("#progress").val(ui.value + "%");
-            },
-            change: function (event, ui) {
-                var translationLimit = $("#progress").val();
-                chrome.storage.sync.set({'TRAN_LIMIT': translationLimit}, function () {
-                });
-            }
-        });
-
-        $("#progress").val($("#application-progress").slider("value") + "%");
-        
+$(document).ready(function() {
+	
+    chrome.storage.sync.get("TRAN_TARGET", function (obj) {
+		if (!obj["TRAN_TARGET"] || obj["TRAN_TARGET"] == "") {
+			$("#setupTarget").css("display", "block");
+			$("#regularTarget").css("display", "none");
+		} else {
+			$("#setupTarget").css("display", "none");
+			$("#regularTarget").css("display", "block");
+			$("#target").val(obj["TRAN_TARGET"]);
+		}
     });
-    
-});
 
-function disappear(event){
-	  
-	event.preventDefault();
-	  
-	document.GetElementsByClassName("question").innerHTML="<p>Thanks you!</p>";
-		  
-	return false;	  
-	 };
+    chrome.storage.sync.get("TRAN_LEVEL", function (obj) {
+		if (!obj["TRAN_LEVEL"] || obj["TRAN_LEVEL"] == "") {
+			$("#setupLevel").css("display", "block");
+			$("#regularLevel").css("display", "none");
+		} else {
+			$("#setupLevel").css("display", "none");
+			$("#regularLevel").css("display", "block");
+			$("#difficulty").val(obj["TRAN_LEVEL"]);
+		}
+    });
+	
+    chrome.storage.sync.get("SPKESY_TRAN", function (obj) {
+    	if (!obj["SPKESY_TRAN"] || obj["SPKESY_TRAN"] === "") {
+    		chrome.storage.sync.set({"SPKESY_TRAN":"ON"}, function() {
+    		});
+			$("#off").css("display", "block");
+			$("#on").css("display", "none");
+		} else if (obj["SPKESY_TRAN"] && obj["SPKESY_TRAN"] === "ON") {
+			$("#off").css("display", "block");
+			$("#on").css("display", "none");
+		} else if (obj["SPKESY_TRAN"] && obj["SPKESY_TRAN"] === "OFF") {
+			$("#on").css("display", "block");
+			$("#off").css("display", "none");
+		}
+    });
+	
+	// Save the selected target language on user's PC
+	$("#target").change(function() {
+		chrome.storage.sync.set({"TRAN_TARGET":$("#target option:selected").val()}, function() {
+		});
+	});
+
+	$("#difficulty").change(function() {
+		chrome.storage.sync.set({"TRAN_LEVEL":$("#difficulty option:selected").val()}, function() {
+		});
+	});
+
+	// Attach a submit handler to feedback form
+	$('#feedbackForm').submit(function(event) {
+		
+		event.preventDefault();
+		
+		var array = $(this).serializeArray();
+		var jsonParameter = {};
+		
+		jQuery.each(array, function() {
+			jsonParameter[this.name] = this.value || '';
+	    });
+		
+		$.ajax({
+			url : "http://ec2-52-35-34-105.us-west-2.compute.amazonaws.com:8080/translator/rest/submit/feedback",
+			type : "POST",
+	        data: JSON.stringify(jsonParameter),
+	        contentType: "application/json",
+	        headers: {"Accept": "application/json"},
+			success: function(result, status, xhr) {
+				$("#feedbackDiv").html(result['message']);
+			},
+			error: function (xhr, status, errorMsg) {
+	            alert(xhr.status + "::" + xhr.statusText + "::" + xhr.responseText);
+	        }
+		});
+	});
+
+	$("#feedbackButton").click(function() {
+		$("#languageDiv").hide("slide", { direction: "left" }, 400);
+	    $("#feedbackDiv").show("slide", { direction: "right" }, 400);
+	});
+	
+	$("#off").click(function() {
+		$("#off").hide();
+	    $("#on").show();
+		chrome.storage.sync.set({"SPKESY_TRAN":"OFF"}, function() {
+		});
+	});
+	
+	$("#on").click(function() {
+		$("#on").hide();
+	    $("#off").show();
+		chrome.storage.sync.set({"SPKESY_TRAN":"ON"}, function() {
+		});
+	});
+	
+	$("#help").click(function() {
+		window.open("http://spkeasy.weebly.com/help-page.html","_blank");
+	});
+	
+	$(".difficultyDescription").tooltip({
+	      position: {
+	          my: "center bottom-15",
+	          at: "center top",
+	          using: function( position, feedback ) {
+	            $( this ).css( position );
+	            $( "<div>" )
+	              .addClass( "arrow" )
+	              .addClass( feedback.vertical )
+	              .addClass( feedback.horizontal )
+	              .appendTo( this );
+	          	}
+	        }
+		});
+});
